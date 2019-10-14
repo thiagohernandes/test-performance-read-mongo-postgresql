0fa355a27ebab47523555d3bae41ade0e793668f
Thiago Hernandes Souza <thiago.hernandes.souza@everis.com>
2019-10-14 16:52:51 -0300

    Criação de projetos

---
+++ postgresql-read-app/src/main/java/com/postgresql/PostgresqlReadAppApplication.java
@@ -0,0 +1,13 @@
+package com.postgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresqlReadAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresqlReadAppApplication.class, args);
+	}
+
+}
